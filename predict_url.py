455418d05bad172e5d2e5fd89edb430e5958d857
Nam Anh <dangnamanh05@gmail.com>
2023-04-24 14:29:55 +0700

    updated predict_url to use nblocker database

--- predict_url.py
+++ predict_url.py
@@ -1,11 +1,13 @@
 import pickle
 import config
-import argparse
+import sqlite3
 from functions import scrape_url
 
 pickle_in = open(config.WORDS_FREQUENCY_PATH, "rb")
 words_frequency = pickle.load(pickle_in)
 
+
+"""
 parser = argparse.ArgumentParser(description='URLs for category predictions')
 parser.add_argument('-u', '--url', help='Predict custom website')
 parser.add_argument('-t', '--text_file_path', help='Predict websites written in text file')
@@ -31,3 +33,33 @@ elif args.text_file_path:
                 print('Predicted submain category:', results[2])
 else:
     parser.error("Please specify websites input type. More about input types you can find 'python predict_url -h'")
+"""
+if __name__ == '__main__':
+    connection =   sqlite3.connect("nblocker.sqlite3")
+    cursor = connection.cursor()
+    rows = cursor.execute("""select id, domain
+                          from domains
+                          where category is null  
+                          order by last_seen desc 
+                          limit 30""").fetchall()
+
+    for i in rows:
+        id = i[0]
+        url = i[1]
+        url = "https://" + url
+        results = scrape_url(url.replace('\n', ''), words_frequency)
+        category = 'undefined'
+        if results:
+            category = results[0]
+        #print(id, url, category)
+        cursor.execute("""update domains
+        set CATEGORY = ? WHERE ID = ?""", (category, id))
+        connection.commit()
+    temp_rows = cursor.execute("""select id, domain, category
+    from domains
+    order by last_seen desc
+    limit 30""").fetchall()
+    for i in temp_rows:
+        print(i)
+    connection.close()
+
